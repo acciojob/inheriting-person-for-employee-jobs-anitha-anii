be726e4ffeaae4f63a7b4c490086a8f87e316ea7
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-13 09:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
+
+function Employee(name, age, jobTitle) {
+Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
+
+let person1 = new Person("harsha", 30);
+person1.greet(); 
+
+let employee1 = new Employee("Anitha", 25, "Engineer");
+employee1.greet(); 
+employee1.jobGreet(); 
+
+	
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
